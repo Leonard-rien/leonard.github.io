9c38f30cf83b3bd7e62d2385bd6701ab750444ac
Léandre <thynva607@gmail.com>
2023-12-28 17:59:21 +0100

    Update writeText.js

--- functions/writeText.js
+++ functions/writeText.js
@@ -8,21 +8,25 @@ app.use(bodyParser.json());
 
 app.post('/writeText', (req, res) => {
   try {
-    console.error('Début de la fonction writeText');
-
-    const texteRecu = req.body;
-    console.error('Texte reçu:', texteRecu);
+    const texteRecu = req.body.texte;
+    console.log('Texte reçu:', texteRecu);
 
     // Traitez le texte comme nécessaire
     // ...
 
     res.status(200).json({ message: 'Texte reçu avec succès.' });
-
-    console.error('Fin de la fonction writeText');
   } catch (error) {
     console.error('Erreur:', error);
     res.status(500).json({ error: 'Une erreur s\'est produite.' });
   }
 });
 
-module.exports.handler = serverless(app);
+module.exports.handler = async (event, context) => {
+  console.log('Function executed with event:', event);
+  // ...
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Function executed successfully.' }),
+  };
+};
