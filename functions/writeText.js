70346d71787666971e77259c10d23faedea30f40
Léandre <thynva607@gmail.com>
2023-12-28 18:23:16 +0100

    retour

--- functions/writeText.js
+++ functions/writeText.js
@@ -9,16 +9,24 @@ app.use(bodyParser.json());
 app.post('/writeText', (req, res) => {
   try {
     const texteRecu = req.body.texte;
+    console.log('Texte reçu:', texteRecu);
 
     // Traitez le texte comme nécessaire
     // ...
 
-    // Renvoyez le texte dans la réponse JSON
-    res.status(200).json({ message: texteRecu });
+    res.status(200).json({ message: 'Texte reçu avec succès.' });
   } catch (error) {
     console.error('Erreur:', error);
     res.status(500).json({ error: 'Une erreur s\'est produite.' });
   }
 });
 
-module.exports.handler = serverless(app);
+module.exports.handler = async (event, context) => {
+  console.log('Function executed with event:', event);
+  // ...
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Function executed successfully.' }),
+  };
+};
