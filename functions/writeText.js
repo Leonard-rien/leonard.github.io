e43c7ec976bbb2018fabd967a308f96db654a522
Léandre <thynva607@gmail.com>
2023-12-28 16:05:53 +0100

    Update writeText.js

--- functions/writeText.js
+++ functions/writeText.js
@@ -1,20 +1,24 @@
 // functions/writeText.js
+const express = require('express');
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
 
-exports.handler = async function (event, context) {
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/writeText', (req, res) => {
   try {
-    // Récupérez le texte à partir du corps de la requête
-    const nouveauTexte = event.body;
+    const texteRecu = req.body.texte;
+    console.log('Texte reçu:', texteRecu);
 
-    // Vous pouvez traiter ou enregistrer le texte comme vous le souhaitez
-    // Dans cet exemple, nous allons simplement le renvoyer
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Le texte a été enregistré avec succès.' }),
-    };
+    // Traitez le texte comme nécessaire
+    // ...
+
+    res.status(200).json({ message: 'Texte reçu avec succès.' });
   } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Une erreur sest produite.' }),
-    };
+    console.error('Erreur:', error);
+    res.status(500).json({ error: 'Une erreur s\'est produite.' });
   }
-};
+});
+
+module.exports.handler = serverless(app);
