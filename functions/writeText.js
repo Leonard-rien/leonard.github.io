8adcc1c5c29f66f01ce860c6355e67b53432856e
Léandre <thynva607@gmail.com>
2023-12-29 10:51:32 +0100

    Update writeText.js

--- functions/writeText.js
+++ functions/writeText.js
@@ -33,7 +33,7 @@ module.exports.handler = async (event, context) => {
 
     return {
       statusCode: 200,
-      body: event.body,
+      body: JSON.stringify({ message: event.body}),
     };
   } catch (error) {
     console.error('Erreur:', error);
